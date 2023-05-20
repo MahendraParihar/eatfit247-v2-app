6d3c6c979de3d926f6900ace854604615f78123f
mahendrakelp <mahendra.p@kelpglobal.com>
2023-05-20 10:16:28 +0530

    layout issue fixed

--- eatfit247-cms-api/src/core/pdf/pdf.service.ts
+++ eatfit247-cms-api/src/core/pdf/pdf.service.ts
@@ -44,7 +44,10 @@ export class PdfService {
     await this.registerHeaderFooter();
 
     const html = await this.getData(templateName, data);
-    const browser = await puppeteer.launch({ headless: 'new' });
+    const browser = await puppeteer.launch({ headless: 'new', args:[
+        '--no-sandbox',
+        '--disable-dev-shm-usage',
+      ] });
     const page = await browser.newPage();
     await page.setContent(html);
     await page.pdf({
