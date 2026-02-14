80b362fa3472f361fecc8a9ef2a62c697dff958c
mahendrakelp <mahendra.p@kelpglobal.com>
2026-02-14 15:38:05 +0530

    admin ui issue fixes

---
+++ eatfit247-web-1/src/app/core/utils/constants.ts
@@ -0,0 +1,46 @@
+import { SocialSiteItem } from '@shared-ui';
+
+export const CONTACT_EMAIL = 'eatfit24by7@gmail.com';
+export const CONTACT_NUMBER = '+91-8591854209';
+export const SOCIAL_LINKS: SocialSiteItem[] = [
+  {
+    link: 'https://www.facebook.com/eatfit24by7',
+    icon: 'facebook',
+    type: 'external'
+  },
+  {
+    link: 'https://www.instagram.com/eatfit24by7',
+    icon: 'instagram',
+    type: 'external'
+  },
+  {
+    link: 'https://www.linkedin.com/company/eatfit24by7',
+    icon: 'linkedin',
+    type: 'external'
+  },
+  {
+    link: 'https://www.pinterest.com/eatfit24by7',
+    icon: 'pinterest',
+    type: 'external'
+  },
+  {
+    link: 'https://t.me/eatfit24by7',
+    icon: 'telegram',
+    type: 'external'
+  },
+  {
+    link: 'https://www.youtube.com/@shwetashahEatfit247',
+    icon: 'youtube',
+    type: 'external'
+  },
+  {
+    type: 'external',
+    icon: 'twitter',
+    link: 'https://twitter.com/eatfit247'
+  },
+  {
+    type: 'external',
+    icon: 'telegram',
+    link: 'https://telegram.me/eatfit247'
+  }
+];
--- eatfit247-web-1/src/app/ui/footer/site-footer.component.ts
+++ eatfit247-web-1/src/app/ui/footer/site-footer.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SocialSiteComponent, SocialSiteItem } from '@shared-ui';
+import { CONTACT_EMAIL, CONTACT_NUMBER, SOCIAL_LINKS } from '../../core/utils/constants';
 
 @Component({
   standalone: true,
@@ -13,38 +14,9 @@ import { SocialSiteComponent, SocialSiteItem } from '@shared-ui';
 export class SiteFooterComponent {
   readonly currentYear = new Date().getFullYear();
 
-  readonly socialLinks: SocialSiteItem[] = [
-    {
-      link: 'https://www.facebook.com/eatfit24by7',
-      icon: 'facebook',
-      type: 'external',
-    },
-    {
-      link: 'https://www.instagram.com/eatfit24by7',
-      icon: 'instagram',
-      type: 'external',
-    },
-    {
-      link: 'https://www.linkedin.com/company/eatfit24by7',
-      icon: 'linkedin',
-      type: 'external',
-    },
-    {
-      link: 'https://www.pinterest.com/eatfit24by7',
-      icon: 'pinterest',
-      type: 'external',
-    },
-    {
-      link: 'https://t.me/eatfit24by7',
-      icon: 'telegram',
-      type: 'external',
-    },
-    {
-      link: 'https://www.youtube.com/@shwetashahEatfit247',
-      icon: 'youtube',
-      type: 'external',
-    },
-  ];
+  readonly socialLinks: SocialSiteItem[] = SOCIAL_LINKS;
+  readonly contactEmail = CONTACT_EMAIL;
+  readonly contactNumber = CONTACT_NUMBER;
 }
 
 
